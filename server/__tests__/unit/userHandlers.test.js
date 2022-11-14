5295be6d5408520133be4dc5e87c1f0c435605ca
Wenjing Zhao <celiazhao89@gmail.com>
2022-11-13 20:30:11 -0500

    set jest config

--- server/__tests__/unit/userHandlers.test.js
+++ server/__tests__/unit/userHandlers.test.js
@@ -1,28 +1,26 @@
 const { validateSpaceTalkerExist } = require("../../handlers/userHandlers");
 
 describe("validateSpaceTalkerExist", () => {
-  // test("when the array is empty", () => {
-  //   const userData = { messages: [] };
+  test("when the array is empty", () => {
+    const userData = { messages: [] };
 
-  //   validateSpaceTalkerExist(userData, {}, {}, 1, 2);
-  //   expect(userData.messages.length).toBe(1);
-  // });
+    validateSpaceTalkerExist(userData, {}, {}, 1, 2);
+    expect(userData.messages.length).toBe(1);
+  });
 
-  // test("when results is false ", () => {
-  //   const userData = { messages: [{ spaceId: 2, talkerId: 1 }] };
+  test("when results is false ", () => {
+    const userData = { messages: [{ spaceId: 2, talkerId: 1 }] };
 
-  //   validateSpaceTalkerExist(userData, {}, {}, 1, 2);
-  //   expect(userData.messages.length).toBe(2);
-  // });
+    validateSpaceTalkerExist(userData, {}, {}, 1, 2);
+    expect(userData.messages.length).toBe(2);
+  });
 
-  // test("when result is true", () => {
-  //   const userData = {
-  //     messages: [{ spaceId: 1, talkerId: 2, messagesLog: [] }],
-  //   };
+  test("when result is true", () => {
+    const userData = {
+      messages: [{ spaceId: 1, talkerId: 2, messagesLog: [] }],
+    };
 
-  //   validateSpaceTalkerExist(userData, {}, {}, 1, 2);
-  //   expect(userData.messages[0].messagesLog.length).toBe(1);
-  // });
-
-  test("", () => {});
+    validateSpaceTalkerExist(userData, {}, {}, 1, 2);
+    expect(userData.messages[0].messagesLog.length).toBe(1);
+  });
 });
